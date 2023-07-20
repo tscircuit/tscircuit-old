1a67946219343c358c26d9c658d7a0f08ad259b7
Severin Ibarluzea <seveibar@gmail.com>
2023-07-19 23:19:16 -0700

    wip

--- src/components/CircuitEditor/CircuitEditor.stories.tsx
+++ src/components/CircuitEditor/CircuitEditor.stories.tsx
@@ -1,6 +1,12 @@
+import React from "react"
 import { CircuitEditor } from "."
+
 export const Test1 = () => {
-  return "Hello World"
+  return (
+    <CircuitEditor>
+      <resistor resistance="10ohm" footprint="0805" sch_x={0} sch_y={0} />
+    </CircuitEditor>
+  )
 }
 
 export default {
--- src/components/CircuitEditor/index.tsx
+++ src/components/CircuitEditor/index.tsx
@@ -1,1 +1,13 @@
-export const CircuitEditor = () => null
+import React from "react"
+import { DebugViewer } from "@tscircuit/debug-viewer"
+import { useRenderedElements } from "@tscircuit/react-fiber"
+import { Schematic } from "@tscircuit/schematic-viewer"
+
+export const CircuitEditor = ({ children }: any) => {
+  return <Schematic>{children}</Schematic>
+  const { elements, loading } = useRenderedElements(children)
+
+  if (loading) return null
+
+  return <DebugViewer soup={elements} schematic />
+}
