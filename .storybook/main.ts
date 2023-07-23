e4e4b71969634c035263081e615a09feacfafaf2
Severin Ibarluzea <seveibar@gmail.com>
2023-07-22 18:15:32 -0700

    switch to webpack, add yalc commit protection

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,13 @@
-import type { StorybookConfig } from "@storybook/react-vite"
+import type { StorybookConfig } from "@storybook/react-webpack5"
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [],
   framework: {
-    name: "@storybook/react-vite",
+    name: "@storybook/react-webpack5",
     options: {},
+  },
+  docs: {
+    autodocs: "tag",
   },
 }
 export default config
