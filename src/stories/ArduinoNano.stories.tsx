7263dcbe5fd1293ba180a6a395df810d039b1e69
Severin Ibarluzea <seveibar@gmail.com>
2023-07-22 23:57:24 -0700

    mostly usable editor

--- src/stories/ArduinoNano.stories.tsx
+++ src/stories/ArduinoNano.stories.tsx
@@ -1,8 +1,21 @@
+import React from "react"
+import { CircuitEditor } from "../components/CircuitEditor"
+
 export const ArduinoNano = () => {
-  return "hi"
+  return (
+    <CircuitEditor>
+      <resistor
+        name="R1"
+        sch_x={0}
+        sch_y={0}
+        footprint="0805"
+        resistance="10kohm"
+      />
+    </CircuitEditor>
+  )
 }
 
 export default {
-  title: "ArduinoNano", 
+  title: "ArduinoNano",
   component: ArduinoNano,
 }
