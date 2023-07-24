52abf9cb4f271965761d7d833ae9c260b8f571e2
Severin Ibarluzea <seveibar@gmail.com>
2023-07-24 00:16:06 -0700

    fix: schematic viewer bug offset

--- src/stories/ArduinoNano/Reg5V.tsx
+++ src/stories/ArduinoNano/Reg5V.tsx
@@ -3,15 +3,16 @@ export const Reg5V = () => {
     <>
       <powersource voltage="5volt" cx={0} cy={0} />
       <bug
-        cx={3}
-        cy={0}
+        sch_x={3}
+        sch_y={0}
         name="LM117IMPX-5.0"
         port_arrangement={{
           bottom_size: 1,
           left_size: 1,
           right_size: 1,
-          top_sizew: 0,
+          top_size: 0,
         }}
+        port_labels={{}}
       />
     </>
   )
