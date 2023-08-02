0a1b82a910aab0036aaf8e397a16dda85126d33f
Severin Ibarluzea <seveibar@gmail.com>
2023-08-01 22:11:41 -0700

    some commented todo features

--- src/stories/ArduinoNano/Reg5V.tsx
+++ src/stories/ArduinoNano/Reg5V.tsx
@@ -2,6 +2,7 @@ export const Reg5V = () => {
   return (
     <>
       <powersource name="5v" voltage="5volt" cx={0} cy={0} />
+      {/* <netalias name="5v" cx={0} cy={0} /> */}
       <bug
         sch_x={3}
         sch_y={0}
@@ -10,9 +11,10 @@ export const Reg5V = () => {
         port_arrangement={{
           bottom_size: 1,
           left_size: 1,
-          right_size: 1,
+          right_size: 2,
           top_size: 0,
         }}
+        // pin_order={[3, 1, 2, 4]}
         port_labels={{
           "1": "vout",
           "2": "gnd",
